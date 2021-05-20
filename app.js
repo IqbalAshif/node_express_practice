0b8d92a8b9cbb8a939e92ff7d42dd03af76ed858
Ashif Iqbal <ashif.iqbal@metropolia.fi>
2021-05-21 02:22:30 +0300

    npm

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-const http = require('http');
+const _ = require('lodash');
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.end('Welcome to  our homepage');
-  } else if (req.url === '/about') {
-    res.end('Here is our short history');
-  } else {
-    res.end(`
-      <h1>Oops!</h1>
-      <p>We cant seem to find what you are looking for</p>
-      <a href="/">Back home</a>
-  `);
-  }
-});
-
-server.listen(5000);
+const items = [1, [2, [3, [4]]]];
